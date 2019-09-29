66586c7af951355b80158c5b83da72654a69c278
Lakshay <lakshay99@rocketmail.com>
2019-09-29 20:02:19 +0530

    Somewhat working

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,11 +1,24 @@
 import numpy as np
 import cv2 as cv
-import os
 import matplotlib.pyplot as plt
+import os
 #Databse of image names with their keypoints and descriptors
 database = {}
+featureDetector = cv.xfeatures2d.SIFT_create()
 #Dictionary of image name and a bool to find whether image is already stitched or not
 # imagesAlreadyStitched = {}
+
+def readEqualisedImage(path):
+  img = cv.imread(path)
+  img_yuv = cv.cvtColor(img, cv.COLOR_BGR2YUV)
+
+  # equalize the histogram of the Y channel
+  img_yuv[:,:,0] = cv.equalizeHist(img_yuv[:,:,0])
+
+  # convert the YUV image back to RGB format
+  img_output = cv.cvtColor(img_yuv, cv.COLOR_YUV2BGR)
+  img_output = cv.resize(img_output, (int(img_output.shape[1]/5), int(img_output.shape[0]/5)))
+  return img_output
 
 #To trim the black protion of the image
 def trimImage(image):
@@ -29,6 +42,7 @@ def warpTwoImages(img1, img2, H):
     '''warp img2 to img1 with homograph H'''
     h1,w1 = img1.shape[:2]
     h2,w2 = img2.shape[:2]
+    print((h2, w2))
     pts1 = np.float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2)
     pts2 = np.float32([[0,0],[0,h2],[w2,h2],[w2,0]]).reshape(-1,1,2)
     pts2_ = cv.perspectiveTransform(pts2, H)
@@ -37,7 +51,7 @@ def warpTwoImages(img1, img2, H):
     [xmax, ymax] = np.int32(pts.max(axis=0).ravel() + 0.5)
     t = [-xmin,-ymin]
     Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) # translate
-
+    print(pts2_)
     result = cv.warpPerspective(img2, Ht.dot(H), (xmax-xmin, ymax-ymin))
     result[t[1]:h1+t[1],t[0]:w1+t[0]] = img1
     # alpha = 0.5
@@ -51,7 +65,7 @@ def imageMatcher(path, img, imgName, imagesFromWhichToSelect):
   Matches all the images in path folder and returns the most similar image, also returns the homography with the best image
   name. 
   """
-  # img = cv.imread('1/2.jpg', cv.IMREAD_GRAYSCALE)
+
   maxSimilarity = 0
   similarImageName = '2.jpg'
   # Keypoints and descriptor of main image
@@ -61,7 +75,6 @@ def imageMatcher(path, img, imgName, imagesFromWhichToSelect):
     # Filename shouldn't be name of this image
 
     # print(fileName)
-    # imgToCompare = cv.imread(path + fileName, cv.IMREAD_GRAYSCALE)
     keyPoints2, d2  = database[(fileName)]
     # print(len(keyPoints1))
     # print(len(keyPoints2))
@@ -87,13 +100,7 @@ def imageMatcher(path, img, imgName, imagesFromWhichToSelect):
       similarImageName = fileName
       best_good_points = list(good_points)
   print(similarImageName)
-  similarImage = cv.imread(path + similarImageName, cv.IMREAD_GRAYSCALE)
-  # draw_params = dict(matchColor = (0,255,0),
-  #                   singlePointColor = (255,0,0),
-  #                   matchesMask = matchesMask,
-  #                   flags = cv.DrawMatchesFlags_DEFAULT)
-  # matchingImage = cv.drawMatchesKnn(img,keyPoints1,similarImage,keyPoints2,matches,None,**draw_params)
-  # plt.imshow(matchingImage),plt.show()
+  similarImage = readEqualisedImage(path + similarImageName)
   # Estimating Homography
   obj = np.empty((len(best_good_points), 2), dtype=np.float32)
   scene = np.empty((len(best_good_points), 2), dtype=np.float32)
@@ -106,83 +113,81 @@ def imageMatcher(path, img, imgName, imagesFromWhichToSelect):
     scene[i, 0] = keyPoints2[best_good_points[i].trainIdx].pt[0]
     scene[i, 1] = keyPoints2[best_good_points[i].trainIdx].pt[1]
 
+  draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = cv.DrawMatchesFlags_DEFAULT)
+  matchingImage = cv.drawMatchesKnn(similarImage,keyPoints2,img,keyPoints1,matches,None,**draw_params)
+  plt.imshow(matchingImage),plt.show()
+
   reprojThresh = 4.0
-  (Homography, status) = cv.findHomography(obj, scene, cv.RANSAC, reprojThresh)
+  (Homography, status) = cv.findHomography(scene, obj, cv.RANSAC, reprojThresh)
 
   return (Homography, status, similarImageName)
 
 def updateDatabase(image, imageName):
   global database
-  # database = {(None, None)} * len(os.listdir(path))
-  # Number of features
-  numberOfFeatures = 5000
-  # feature detector
-  featureDetector = cv.ORB_create(nfeatures=numberOfFeatures)
+  global featureDetector
+
   keyPoints, d = featureDetector.detectAndCompute(image, None)
   database[imageName] = (keyPoints, d)
 
 def calculateDatabase(path):
   global database
-  # database = {(None, None)} * len(os.listdir(path))
-  # Number of features 
-  numberOfFeatures = 5000
-  # feature detector
-  featureDetector = cv.ORB_create(nfeatures = numberOfFeatures)
+  global featureDetector
+
   for fileName in os.listdir(path):
     # Reading image
-    img = cv.imread(path + fileName, cv.IMREAD_GRAYSCALE)
+    img = readEqualisedImage(path + fileName)
     print(img.shape)
     keyPoints, d  = featureDetector.detectAndCompute(img, None)
     database[fileName] = (keyPoints, d)
 
 if __name__ == '__main__':
   #TODO: Resize images
-  # global imagesAlreadyStitched
   path = '1/'
   calculateDatabase(path)
   fileNameList = os.listdir(path)
   baseFileName = fileNameList[0]
-  # imagesAlreadyStitched[baseFileName] = True
   imagesRemainingToBeStitched = list(fileNameList[1:])
   #Will keep modifying this baseImage by stitching images to it one by one
-  baseImg = cv.imread(path + baseFileName, cv.IMREAD_GRAYSCALE)
+  baseImg = readEqualisedImage(path + baseFileName)
   baseImgName = "Base"
   updateDatabase(baseImg, baseImgName)
-  # imageMatcher(path, baseImg, )
-  # for fileName in imagesRemainingToBeStitched:
-  #   imageMatcher(path, baseImg, )
+
   while not(len(imagesRemainingToBeStitched) == 0): #Till no remaining images
     (Homography, status, similarImageName) = imageMatcher(path, baseImg, baseImgName, imagesRemainingToBeStitched)
     #Removing the similar image from imagesRemainingToBeStitched
     print(similarImageName)
+    print(Homography)
     len_temp = len(imagesRemainingToBeStitched)
     imagesRemainingToBeStitched = list(image for image in imagesRemainingToBeStitched if not(image == similarImageName))
 
     if(len(imagesRemainingToBeStitched) == len_temp):
       break
 
-    similarImage = cv.imread(path + similarImageName, cv.IMREAD_GRAYSCALE)
+    similarImage = readEqualisedImage(path + similarImageName)
     if baseImg is None:
       print("Yes")
     else:
       print("No")
 
-    result = warpTwoImages(similarImage, baseImg, Homography)
-    # result = cv.warpPerspective(baseImg, Homography,
-    #                              (baseImg.shape[1] + similarImage.shape[1], baseImg.shape[0]))
-    result[0:similarImage.shape[0], 0:similarImage.shape[1]] = similarImage
 
+    result = warpTwoImages(baseImg, similarImage, Homography)
+    # result = cv.warpPerspective(similarImage, Homography, (similarImage.shape[1] + baseImg.shape[1], similarImage.shape[0]))
+    # result[0:baseImg.shape[0], 0:baseImg.shape[1]] = baseImg
+	
     result = trimImage(result)
-    baseImg = cv.resize(result, (similarImage.shape[1], similarImage.shape[0]))
-    baseImg1 = cv.resize(result, (int(baseImg.shape[1] / 10), int(baseImg.shape[0] / 10)))
+    baseImg = result
+    # baseImg = cv.resize(result, (similarImage.shape[1], similarImage.shape[0]))
+    baseImg1 = cv.resize(result, (416, 234))
 
 
     updateDatabase(baseImg, baseImgName)
-    # cv.imshow("Result", baseImg1)
-    # cv.waitKey(0)
+    cv.imshow("Result", baseImg1)
+    cv.waitKey(0)
     print(baseImg.shape)
     print(similarImage.shape)
 
-  baseImg = cv.resize(baseImg, (int(baseImg.shape[1]/10), int(baseImg.shape[0]/10)))
-  cv.imshow("Result", baseImg)
-  cv.waitKey(0)
+  baseImg = cv.resize(baseImg, (416, 234))
+  cv.imwrite("Result.jpg", baseImg)
