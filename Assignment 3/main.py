38216018e410d4adf91178ad3b8bcf5d3c9c7675
Overlord <overlord@ubuntu.ubuntu-domain>
2019-10-23 02:40:26 +0530

    Doing movement, but noisy, some testing remaining

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -10,27 +10,106 @@ orb = cv2.ORB_create()
 # create brute force matcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)  
 
+# Database of keypoints and descriptors for the markers, (marker_name, (kp, des)) pair
+keyPointDataBase = {}
 
-def findKeyPoints(fileName):
-  global orb
 
-  model = cv2.imread(fileName, 0)
-  # object to be projected
+# Updates the database, MIN_MATCHES is 10 by default
+def updateDataBase(markerName, MIN_MATCHES = 10):
+  
+  global keyPointDataBase
 
-  MIN_MATCHES = 10
+  markerModel = cv2.imread(markerName, 0)
+
 
   # Compute model keypoints and its descriptors
-  kp_model, des_model = orb.detectAndCompute(model, None)  
-  # intrinsicMatrix = np.load("calibration.npy")
-  return (kp_model, des_model)
+  kp_model, des_model = orb.detectAndCompute(markerModel, None) 
+
+  # Update the keypoint and descriptor database
+  keyPointDataBase[markerName] = (kp_model, des_model)
+
+
+# Initialises the database of keypoints and descriptors of the markers
+def initialiseDataBase(markerNameList, MIN_MATCHES = 10):
+  
+  for markerName in markerNameList:
+    updateDataBase(markerName, MIN_MATCHES)
+
+
+# Finds the midpoint of the given marker inside the video frame and returns it as numpy array, returna empty array if not enough matches
+def findMarkerMidPointInFrame(frameKeyPointDescriptorTuple, markerModelShape, markerName, MIN_MATCHES = 10):
+  
+  midPointArray = np.array([])
+  homography = None
+
+  # Obtaining the keypoints and descriptors of the frame
+  kp_frame, des_frame = frameKeyPointDescriptorTuple
+
+  # Obtaining the keypoints and descriptors of the marker from the database
+  kp_marker, des_marker = keyPointDataBase[markerName]
+
+  # Finding the matches between the frame and the markerImage
+  matches = bf.match(des_marker, des_frame)
+  # Sorting out good matches
+  matches = sorted(matches, key=lambda x: x.distance)
+  # TODO: Hardcoded, need a better measure
+  maxDistance = 30
+  # Number of good matches
+  good_matches = [m for m in matches if m.distance < maxDistance]
+
+  #Checking for enough matches
+  if len(good_matches) > MIN_MATCHES:
+    # differentiate between source points and destination points
+    src_pts = np.float32([kp_marker[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    # compute Homography
+    reprojThresh = 4.0
+    homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, reprojThresh)
+    # draw matches.
+    # frame = cv2.drawMatches(model, kp_model, frame, kp_frame,
+    #                       matches[:MIN_MATCHES], 0, flags=2)
+
+    if homography is not None:
+      h, w = markerModelShape
+      pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+      # project corners into frame
+      dst = cv2.perspectiveTransform(pts, homography)
+      # # connect them with lines  
+      # frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA) 
+
+      # Finding the midPoint of the four points obtained
+      midPointArray = np.array( [ np.mean( dst[:, 0, 0] ), np.mean( dst[:, 0, 1] ) ] )
+    else:
+      print("Homography couldnt be computed for marker %s" % (markerName))
+
+  else:
+    # Not enough matches
+    print ("Not enough matches for marker %s have been found - %d/%d" % ( markerName, len(good_matches),
+                                                            MIN_MATCHES))
+
+  return midPointArray
+
+
+''' Returns a unit vector in the specified direction of the vector'''
+def normalize(v):
+    norm=np.linalg.norm(v, ord=2)
+    if norm==0:
+      return v
+    return v/norm
+
 
 def featureMatching(sourceMarkerFileName, destinationMarkerFileName):
   ''' Match the features of model to scene '''
 
-
+  # For source marker model
   model = cv2.imread(sourceMarkerFileName, 0)
-  source_kp_model, source_des_model = findKeyPoints(sourceMarkerFileName)
-  # destination_kp_model, destination_des_model = findKeyPoints(destinationMarkerFileName)
+  source_kp_model, source_des_model = keyPointDataBase[sourceMarkerFileName]
+
+  # For destination marker model
+  destinationMarkerModel = cv2.imread(destinationMarkerFileName, 0)
+  destination_kp_model, destination_des_model = keyPointDataBase[destinationMarkerFileName]
+  
+  
   MIN_MATCHES = 10
 
   obj = OBJ('fox.obj', swapyz=True)
@@ -40,8 +119,16 @@ def featureMatching(sourceMarkerFileName, destinationMarkerFileName):
 
   cap = cv2.VideoCapture(0)
 
+  # Setting the kinematic properties of the object
   objectOffset = (0,0,0)
+  objectDistanceFromSource = 0.0
+  objectSpeed = 0.5
+
   while True:
+    
+    #Initialising homography as None
+    homography = None
+
     # read the current frame
     ret, frame = cap.read()
     if not ret:
@@ -54,12 +141,12 @@ def featureMatching(sourceMarkerFileName, destinationMarkerFileName):
     matches = sorted(matches, key=lambda x: x.distance)
     # Sorting out good matches
     # TODO: Hardcoded, need a better measure
-    maxDistance = 40
+    maxDistance = 30
     # Number of good matches
     good_matches = [m for m in matches if m.distance < maxDistance]
     
     if len(good_matches) > MIN_MATCHES:
-      # differenciate between source points and destination points
+      # differentiate between source points and destination points
       src_pts = np.float32([source_kp_model[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
       dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
       # compute Homography
@@ -68,30 +155,68 @@ def featureMatching(sourceMarkerFileName, destinationMarkerFileName):
       # draw matches.
       # frame = cv2.drawMatches(model, kp_model, frame, kp_frame,
       #                       matches[:MIN_MATCHES], 0, flags=2)
-
-      h, w = model.shape
-      pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-      # project corners into frame
-      dst = cv2.perspectiveTransform(pts, homography)
-      # connect them with lines  
-      frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA) 
       
       # if a valid homography matrix was found render cube on model plane
       if homography is not None:
+        h, w = model.shape
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        # project corners into frame
+        dst = cv2.perspectiveTransform(pts, homography)
+        # connect them with lines  
+        frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA) 
+
+        # Finding the midpoints for source and destination markers
+        sourceMarkerMidPointArray = np.array( [ np.mean( dst[:, 0, 0] ), np.mean( dst[:, 0, 1] ) ] )
+        destinationMarkerMidPointArray = findMarkerMidPointInFrame((kp_frame, des_frame), destinationMarkerModel.shape, destinationMarkerFileName)
+
+
+        doesDirectionVectorExist = False
+
+        if not destinationMarkerMidPointArray.size == 0:
+          directionVectorForObject = destinationMarkerMidPointArray - sourceMarkerMidPointArray
+          unitDirectionVectorForObject = normalize(directionVectorForObject)
+          doesDirectionVectorExist = True
+
+        #If closer than this, then stop updating
+        objectDistancefromDestinationThreshold = 5.0
+        
+        if doesDirectionVectorExist:
+          # Updating the object distance from source
+          objectDistanceFromSource += objectSpeed
+          # Updating the object offset coordinates
+          object2DOffsetCoordinates = objectDistanceFromSource * unitDirectionVectorForObject
+          # Updating the object 2D coordinates
+          object2DCoordinates = sourceMarkerMidPointArray + object2DOffsetCoordinates
+
+          # Calculating object distance from destination
+          vector = (destinationMarkerMidPointArray - object2DCoordinates)
+          vectorNorm = np.linalg.norm(vector, ord=2)
+          objectDistancefromDestination = vectorNorm ** 2
+
+          # If less than threshold, stop moving
+          if objectDistancefromDestination < objectDistancefromDestinationThreshold:
+            objectOffset = ( int(object2DOffsetCoordinates[0]), int(object2DOffsetCoordinates[1]), objectOffset[0]) 
+
+
+
         # obtain 3D projection matrix from homography matrix and camera parameters
         projection = projection_matrix(intrinsicMatrix, homography)  
         # project cube or model
-        frame = render(frame, obj, projection, model, objectOffset, False)                     
+        frame = render(frame, obj, projection, model, objectOffset, False) 
+
+        
+
+        # if doesDirectionVectorExist and objectDistancefromDestination < objectDistancefromDestinationThreshold :
+        #   objectOffset = ((objectOffset[0] + deltaDisplacementForObject[0]), (objectOffset[1] + deltaDisplacementForObject[1]), objectOffset[2])
+
+      else:
+        print("Homography couldnt be computed for marker %s" % (sourceMarkerFileName))                    
       # show result
       # cv2.imshow('frame', frame)
       # cv2.waitKey(0)
     else:
-      print ("Not enough matches have been found - %d/%d" % (len(good_matches),
+      print ("Not enough matches for marker %s have been found - %d/%d" % (sourceMarkerFileName, len(good_matches),
                                                             MIN_MATCHES))
-    if objectOffset[0] < 50:
-      objectOffset = (objectOffset[0], objectOffset[1] + 1, objectOffset[2])
-    else:
-      objectOffset = (0, 0, 0)
     # show result
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -157,7 +282,12 @@ def render(img, obj, projection, model, positionOffset = (0, 0, 0), color=False)
 
 if __name__ == "__main__":
   
+  # Selecting the source and destination markers to be used
   source_marker = "marker_photos/marker_2.jpg"
-  destination_marker = "marker_photos/marker_3.jpg"
+  destination_marker = "marker_photos/circularMarker.jpg"
+
+  # Initialing the marker keypoint database with source and destination markers
+  initialiseDataBase([source_marker, destination_marker])
   
+  # Computes and renders the augmented image
   featureMatching(source_marker, destination_marker)
