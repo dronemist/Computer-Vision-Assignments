e2e687514c8a22e85e808767d8aceb7dad862a51
dronemist <siddhant.mago@gmail.com>
2019-11-04 22:15:43 +0530

    started assignment 4

---
+++ Assignment 4/Model.py
@@ -0,0 +1,46 @@
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras import backend as K
+
+class GestureRecognizer:
+  def build(width, height, depth, classes):
+
+    # initialize the model along with the input shape to be
+    # "channels last" and the channels dimension itself
+    model = Sequential()
+    inputShape = (height, width, depth)
+    chanDim = -1
+
+    # if we are using "channels first", update the input shape
+    # and channels dimension
+    if K.image_data_format() == "channels_first":
+      inputShape = (depth, height, width)
+      chanDim = 1
+
+    # Making the first model
+    # CONV => CONV => MAXPOOL => CONV => MAXPOOL
+    model.add(Conv2D(32, (3, 3), padding="same", activation='relu', input_shape=inputShape))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(Conv2D(32, (3, 3), padding="same", activation='relu'))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25)) 
+
+    model.add(Conv2D(64, (3, 3), padding="same", activation='relu'))
+    model.add(BatchNormalization(axis=chanDim))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25)) 
+
+    # softmax classifier
+    model.add(Flatten())
+    model.add(Dense(classes))
+    model.add(Activation("softmax"))
+    
+    return model
---
+++ Assignment 4/VideoRecorder.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+import sys
+
+def createDatabase(path, sizeOfImage, imageName, numberOfImages):
+  '''Adds numberOfImages images to path folder'''
+  cap = cv2.VideoCapture(0)
+  count = numberOfImages
+  while count > 0:
+    # Reading frame by frame
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, sizeOfImage)
+    cv2.imwrite(path + '/' + imageName + str(count) + '.jpg', frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    count -= 1
+
+if __name__ == "__main__":
+  createDatabase(sys.argv[1], (50, 50), sys.argv[2], 500)
+  
