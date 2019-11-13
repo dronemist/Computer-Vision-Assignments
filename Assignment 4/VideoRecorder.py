4497e73e09472018bad374c57ff6902c84500cea
dronemist <siddhant.mago@gmail.com>
2019-11-13 18:17:23 +0530

    changed videoRecorder

--- Assignment 4/VideoRecorder.py
+++ Assignment 4/VideoRecorder.py
@@ -3,22 +3,35 @@ import cv2
 import sys
 from preprocess import preprocess
 
+# variables
+bgSubThreshold = 50
+  # bool, whether the background captured
+
 def createDatabase(path, sizeOfImage, imageName, numberOfImages, imagesToSkip):
   '''Adds numberOfImages images to path folder'''
+  isBgCaptured = 0 
   cap = cv2.VideoCapture(0)
   count = numberOfImages
   while count > 0:
     # Reading frame by frame
     ret, frame = cap.read()
-    # cv2.imshow('frame', frame)
-    frame = cv2.resize(frame, sizeOfImage)
-    if count % imagesToSkip == 0: # Images to skip denotes that we have to take every ith image
-      cv2.imwrite(path + '/' + imageName + str(count) + '.jpg', frame)
-    print(count)
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    count -= 1 
+    cv2.imshow('frame1', frame)
+    if isBgCaptured:
+      # cv2.imshow('frame', frame)
+      frame = cv2.resize(frame, sizeOfImage)
+      if count % imagesToSkip == 0: # Images to skip denotes that we have to take every ith image
+        cv2.imwrite(path + '/' + imageName + str(count) + '.jpg', frame)
+      print(count)
+      cv2.imshow('frame', frame)
+      count -= 1 
+    k = cv2.waitKey(10)
+    if k == 27:  # press ESC to exit
+      break
+    elif k == ord('b'):
+      # bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+      isBgCaptured = 1
+      cv2.imwrite(path + '/' + imageName + str(count) + 'bg.jpg', frame)
+      print('!!!Background Captured!!!')
   cap.release()
   cv2.destroyAllWindows()  
 
